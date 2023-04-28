286880ff3276aa648c7ea8375cab3cdab5e37f95
o-ifeanyi <onuifeanyi95@gmail.com>
2023-04-28 15:11:21 +0100

    Initial Commit

---
+++ People/PeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleApp.swift
+//  People
+//
+//  Created by Ifeanyi Onuoha on 28/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct PeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
